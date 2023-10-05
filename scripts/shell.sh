291119a57b74f0bc6ee8d7aba3a7625754061002
VyniMarques <89865087+VyniMarques@users.noreply.github.com>
2023-10-05 20:04:04 -0300

    Update script

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -3,4 +3,4 @@ cd Aula-GitHub-Actions
 ls
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
+echo "Pipeline executado!" | mail -s "a subject" ${EMAIL_LIST}
